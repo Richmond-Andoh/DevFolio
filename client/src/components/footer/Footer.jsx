4dbecd3db67d567cf0f38870d15a5d3e442e5101
richmond-andoh <richmondandoh00@gmail.com>
2024-04-13 03:05:32 +0000

    Index html updated

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,17 +1,15 @@
-import { MdFavorite } from "react-icons/md";
+//import { MdFavorite } from "react-icons/md";
 
 const Footer = () => {
   return( 
   <div className="py-10">
     <div className="h-[1px] w-full bg-gray-700"></div>
-    <div className="flex flex-col items-center justify-center py-5">
+    <div className="flex flex-col items-center justify-center py-10 opacity-90">
         <h1 className="text-white">
-            <span>Designed and Developed With By</span>
-            <span className="text-red">
-                <MdFavorite size={24} />
-            </span>
+            <span>Designed and Developed By</span>
+           
         </h1>
-        <h1 className="text-white">
+        <h1 className="text-teal-300">
             <span>Richmond Andoh</span>
         </h1>
     </div>
