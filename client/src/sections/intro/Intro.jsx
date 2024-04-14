f04761185037d82e534d8e0caeb5e0e85c4489f3
richmond-andoh <richmondandoh00@gmail.com>
2024-04-14 19:14:11 +0000

    Some fixes

--- client/src/sections/intro/Intro.jsx
+++ client/src/sections/intro/Intro.jsx
@@ -1,25 +1,27 @@
 const Intro = () => {
   return (
-    <div className="h-[80vh] bg-primary-content flex flex-col items-start justify-center gap-9 py-10">
-      <h1 className="text-white mt-12 pt-12 font-semibold">Hi There, I am</h1>
-      <h1 className="text-primary text-7xl sm:text-3xl md:text-4xl font-semibold">
-        Richmond Manu Andoh
-      </h1>
-      <h1 className="text-white text-6xl sm:text-2xl md:text-3xl font-semibold">
-        I Build Things For The Web.
-      </h1>
-      <p className="text-white w-2/3">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id deserunt
-        exercitationem eius, rerum architecto earum ex dolore minus consequatur
-        totam maiores culpa odit
-      </p>
-      <div className="flex gap-5">
-        <button className="text-teal border border-teal px-9 py-4 text-center">
-          Get Started
-        </button>
-        <button className="text-teal border border-teal px-9 py-4 text-center">
-          Hire Me
-        </button>
+    <div className="flex w-full">
+      <div className="h-[80vh] bg-primary-content flex flex-col items-start justify-center gap-9 py-10">
+        <h1 className="text-white mt-12 pt-12 font-semibold">Hi There, I am</h1>
+        <h1 className="text-primary text-7xl sm:text-3xl md:text-4xl font-semibold">
+          Richmond Manu Andoh
+        </h1>
+        <h1 className="text-white text-6xl sm:text-2xl md:text-3xl font-semibold">
+          I Build Things For The Web.
+        </h1>
+        <p className="text-white w-2/3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id deserunt
+          exercitationem eius, rerum architecto earum ex dolore minus
+          consequatur totam maiores culpa odit
+        </p>
+        <div className="flex gap-5">
+          <button className="text-teal border border-teal px-7 py-3 text-center">
+            Download CV
+          </button>
+          <button className="text-teal border border-teal px-7 py-3 text-center">
+            Hire Me
+          </button>
+        </div>
       </div>
     </div>
   );
