722c4bb91dc6ae479eaffec8667c9cbc99c2b088
richmond-andoh <richmondandoh00@gmail.com>
2024-04-14 20:42:31 +0000

    Profile Image added

--- client/src/sections/intro/Intro.jsx
+++ client/src/sections/intro/Intro.jsx
@@ -1,12 +1,14 @@
+import Profile from "../../../public/profileImg.png"
+
 const Intro = () => {
   return (
-    <div className="flex w-full">
-      <div className="h-[80vh] bg-primary-content flex flex-col items-start justify-center gap-9 py-10">
+    <div className="flex w-full items-center justify-center sm:flex-col-reverse sm:items-center sm:justify-center">
+      <div className="h-[100vh] w-4/6 bg-primary-content flex flex-col items-start justify-center gap-9 py-10 sm:w-auto sm:h-auto">
         <h1 className="text-white mt-12 pt-12 font-semibold">Hi There, I am</h1>
-        <h1 className="text-primary text-7xl sm:text-3xl md:text-4xl font-semibold">
+        <h1 className="text-primary text-6xl sm:text-3xl md:text-4xl font-semibold">
           Richmond Manu Andoh
         </h1>
-        <h1 className="text-white text-6xl sm:text-2xl md:text-3xl font-semibold">
+        <h1 className="text-white text-5xl sm:text-2xl md:text-3xl font-semibold">
           I Build Things For The Web.
         </h1>
         <p className="text-white w-2/3">
@@ -22,6 +24,9 @@ const Intro = () => {
             Hire Me
           </button>
         </div>
+      </div>
+      <div className="w-2/6 sm:mt-12 sm:h-auto sm:w-auto">
+        <img src={ Profile } alt="" />
       </div>
     </div>
   );
