b6100456dd65567f9a6a7b211ccba69963a7fc71
richmond-andoh <richmondandoh00@gmail.com>
2024-04-16 05:22:49 +0000

    some fixes in the controller

--- server/controllers/getPortfolioData.js
+++ server/controllers/getPortfolioData.js
@@ -1,12 +1,18 @@
-import { Intro, Education, Experience, Project, Contact, About } from "../modules/portfolio"
+// import { Intro, Education, Experience, Project, Contact, About } from "../modules/portfolio"
+import Intro from "../modules/IntroSchema.js";
+import About from "../modules/AboutSchema.js";
+import Experience from "../modules/ExperienceSchema.js";
+import Project from "../modules/ProjectSchema.js";
+import Education from "../modules/EducationSchema.js";
+import Contact from "../modules/ContactSchema.js";
 
 export const getPortfolioData = async (req, res) => {
   try {
     const intro = await Intro.findOne();
     const about = await About.findOne();
-    const experiences = await Experience.find().sort({ order: -1 });
-    const projects = await Project.find().sort({ order: -1 });
-    const education = await Education.find().sort("year", "desc");
+    const experiences = await Experience.find(); //.sort({ order: -1 });
+    const projects = await Project.find(); //.sort({ order: -1 });
+    const education = await Education.find(); //.sort("year", "desc");
     const contact = await Contact.findOne();
 
     if (
--- server/modules/ContactSchema.js
+++ server/modules/ContactSchema.js
@@ -1,6 +1,6 @@
 import mongoose, { model } from "mongoose";
 
-const contactSchema = new  mongoos.Schema({
+const contactSchema = new  mongoose.Schema({
     name: {
         type: String,
         required: true
