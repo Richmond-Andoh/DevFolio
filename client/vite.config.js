3bb72c64bfcab49762eb0306472d0f24672d551e
richmond-andoh <richmondandoh00@gmail.com>
2024-04-18 04:22:53 +0000

    Vite config file updates

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3100,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5050"
+      },
+    },
+  },
 })
