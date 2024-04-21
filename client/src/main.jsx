db4bbfc45032d4541a6eee5f3a7cfe3ca70e1929
richmond-andoh <richmondandoh00@gmail.com>
2024-04-21 05:20:51 +0000

    Strict mode ignored

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,9 +5,7 @@ import './index.css'
 import { Provider } from 'react-redux';
 import store from './reducer/store.js';
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>
 )
