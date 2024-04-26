45ea772434297d6a580fc32667f5288f89691393
richmond-andoh <richmondandoh00@gmail.com>
2024-04-26 20:50:05 +0000

    Admin folder and file added

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,19 +1,19 @@
 //import { MdFavorite } from "react-icons/md";
 
 const Footer = () => {
-  return (
-    <div className="py-10">
-      <div className="h-[1px] w-full bg-gray-700"></div>
-      <div className="flex flex-col items-center justify-center py-10 opacity-90">
+  return( 
+  <div className="py-10">
+    <div className="h-[1px] w-full bg-gray-700"></div>
+    <div className="flex flex-col items-center justify-center py-10 opacity-90">
         <h1 className="text-white">
-          <span>Designed and Developed By</span>
+            <span>Designed and Developed By</span>
+           
         </h1>
         <h1 className="text-teal-300">
-          <span>Richmond Andoh</span>
+            <span>Richmond Andoh</span>
         </h1>
-      </div>
     </div>
-  );
-};
+  </div>
+)};
 
 export default Footer;
