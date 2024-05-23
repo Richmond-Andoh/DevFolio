c79933f0703c999c7b9262f83d5a9a05a0c198e6
richmond-andoh <richmondandoh00@gmail.com>
2024-05-23 19:42:56 +0000

    Some fixes in the update about controller

--- server/controllers/updateAbout.js
+++ server/controllers/updateAbout.js
@@ -11,16 +11,16 @@ const updateAbout = async(req, res) => {
 
      if(!about){
        res.status(404).send({
-         success: false,
-         message: "About Not found"
+        success: false,
+        message: "About Not found"
          
        });
      }
   
      res.status(200).send({
-         data: about,
-         success: true,
-         message: "Intro updated successfully"
+        data: about,
+        success: true,
+        message: "About updated successfully"
      });
     } catch (error) {
      res.status(500).send(error.message);
